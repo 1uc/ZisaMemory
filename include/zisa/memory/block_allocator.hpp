24c8b7ccdc669aea735ddf584379c994c2f7dcc9
Luc Grosheintz <luc.grosheintz@gmail.com>
2019-02-15 14:16:28 +0100

    Implement a block_allocator.

---
+++ include/zisa/memory/block_allocator.hpp
@@ -0,0 +1,126 @@
+/*
+ *
+ */
+
+#ifndef BLOCK_ALLOCATOR_H_U74O6
+#define BLOCK_ALLOCATOR_H_U74O6
+
+#include <exception>
+#include <memory>
+#include <tuple>
+
+#include <zisa/config.hpp>
+
+namespace zisa {
+
+template <class T>
+class block_allocator;
+
+template <class T>
+class locked_ptr {
+public:
+  locked_ptr() : data(nullptr), index(0), allocator(nullptr) {}
+
+  locked_ptr(T *const data,
+             std::size_t index,
+             std::shared_ptr<block_allocator<T>> allocator)
+      : data(data), index(index), allocator(std::move(allocator)) {}
+
+  locked_ptr(const locked_ptr &other) = delete;
+
+  locked_ptr(locked_ptr &&other) { *this = std::move(other); }
+
+  ~locked_ptr() {
+    if (data != nullptr && allocator != nullptr) {
+      allocator->release(*this);
+    }
+  }
+
+  void operator=(const locked_ptr &other) = delete;
+  void operator=(locked_ptr &&other) {
+
+    data = other.data;
+    other.data = nullptr;
+
+    index = other.index;
+    other.index = 0;
+
+    allocator = std::move(other.allocator);
+    other.allocator = nullptr;
+  }
+
+  T *operator->() { return data; }
+  T const *operator->() const { return data; }
+
+  T &operator*() { return *data; }
+  const T &operator*() const { return *data; }
+
+private:
+  T *data;
+
+  std::size_t index;
+  std::shared_ptr<block_allocator<T>> allocator;
+
+  friend class block_allocator<T>;
+};
+
+template <class T>
+class block_allocator
+    : public std::enable_shared_from_this<block_allocator<T>> {
+public:
+  block_allocator(std::size_t max_elements) {
+    free_list.reserve(max_elements);
+    large_block.reserve(max_elements);
+  }
+
+  ~block_allocator() {
+    for (auto &ptr : large_block) {
+      delete ptr;
+    }
+  }
+
+  template <class... Args>
+  locked_ptr<T> allocate(Args &&... args) {
+    auto [index, allocated] = acquire();
+
+    if (!allocated) {
+      large_block[index] = new T(std::forward<Args &&>(args)...);
+    }
+
+    return locked_ptr<T>(large_block[index], index, this->shared_from_this());
+  }
+
+  std::tuple<std::size_t, bool> acquire() {
+    // TODO implement thread safety.
+    // acquire a lock.
+
+    if (free_list.empty()) {
+
+      if (large_block.size() == large_block.capacity()) {
+        throw std::length_error("Exceeding capacity of the block_allocator.");
+      }
+
+      large_block.push_back(nullptr);
+      return std::tuple(large_block.size() - 1, false);
+    }
+
+    auto index = free_list.back();
+    free_list.pop_back();
+    return std::tuple(index, true);
+  }
+
+  void release(const locked_ptr<T> &ptr) {
+    // TODO implement thread safety.
+    // acquire a lock.
+
+    free_list.push_back(ptr.index);
+  }
+
+private:
+  std::vector<int> free_list;
+  std::vector<T *> large_block;
+};
+
+} // namespace zisa
+
+#endif /* end of include guard */
