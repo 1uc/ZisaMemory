69a2f2275a2c9ef068ea5aa8dc356a587c5cc06c
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-02-10 19:35:31 +0100

    Add tests for `array(..., device_type::cuda)`.

--- test/zisa/unit_test/memory/array.cpp
+++ test/zisa/unit_test/memory/array.cpp
@@ -6,9 +6,8 @@
 
 using namespace zisa;
 
-bool implicit_conversion(const array_const_view<double, 3> &view) {
-
-  return view.raw() != nullptr;
+bool implicit_conversion(const array_const_view<double, 3> &view, double *ptr) {
+  return view.raw() == ptr;
 }
 
 TEST_CASE("array; basics", "[array]") {
@@ -23,6 +22,8 @@ TEST_CASE("array; basics", "[array]") {
   auto bc_view = array_const_view<double, 3>(shape(b), raw_ptr(b));
 
   REQUIRE(b.raw() == b_view.raw());
+  REQUIRE(b.raw() == bb_view.raw());
+  REQUIRE(b.raw() == bc_view.raw());
 
   b(1, 0, 1) = -42.0;
   REQUIRE(b_view(1, 0, 1) == -42.0);
@@ -30,7 +31,7 @@ TEST_CASE("array; basics", "[array]") {
   b_view(0, 0, 0) = 42.0;
   REQUIRE(b(0, 0, 0) == 42.0);
 
-  REQUIRE(implicit_conversion(a));
+  REQUIRE(implicit_conversion(a, a.raw()));
 }
 
 TEST_CASE("array; write to file", "[array]") {
@@ -42,7 +43,7 @@ TEST_CASE("array; write to file", "[array]") {
 
   auto a = array<double, 3>(shape);
   for (zisa::int_t i = 0; i < a.size(); ++i) {
-    a[i] = i;
+    a[i] = double(i);
   }
 
   {
@@ -73,6 +74,27 @@ TEST_CASE("array; builds for general Indexing.", "[array]") {
 
   auto a = array<double, 3, column_major>(shape);
   for (zisa::int_t i = 0; i < a.size(); ++i) {
-    a[i] = i;
+    a[i] = double(i);
   }
 }
+
+#if ZISA_HAS_CUDA == 1
+
+TEST_CASE("array; array_view (cuda)", "[cuda][array]") {
+  auto a = array<double, 3>({3, 3, 2}, device_type::cuda);
+  auto b = array<double, 3>({3, 3, 2}, device_type::cuda);
+
+  REQUIRE(a.raw() != static_cast<const decltype(b) &>(b).raw());
+  REQUIRE(a.shape() == b.shape());
+
+  auto b_view = array_view<double, 3>(b);
+  auto bb_view = array_view<double, 3>(shape(b), raw_ptr(b));
+  auto bc_view = array_const_view<double, 3>(shape(b), raw_ptr(b));
+
+  REQUIRE(b.raw() == b_view.raw());
+  REQUIRE(b.raw() == bb_view.raw());
+  REQUIRE(b.raw() == bc_view.raw());
+  REQUIRE(implicit_conversion(a, a.raw()));
+}
+
+#endif
