480f1f68ac772db832d5273d5dac5dfc0bb21953
TobiasRohner <torohner@student.ethz.ch>
2021-03-11 20:49:20 +0100

    Add array constructor from allocator

--- include/zisa/memory/array_impl.hpp
+++ include/zisa/memory/array_impl.hpp
@@ -20,6 +20,10 @@ array<T, n_dims, Indexing>::array(const shape_type &shape, device_type device)
     : super(shape, contiguous_memory<T>(product(shape), device)) {}
 
 template <class T, int n_dims, template <int N> class Indexing>
+array<T, n_dims, Indexing>::array(const shape_type &shape, const allocator<T> &alloc)
+    : super(shape, contiguous_memory<T>(product(shape), alloc)) {}
+
+template <class T, int n_dims, template <int N> class Indexing>
 array<T, n_dims, Indexing>::array(
     const array_const_view<T, n_dims, Indexing> &other)
     : super(shape, contiguous_memory<T>(product(other.shape()))) {
