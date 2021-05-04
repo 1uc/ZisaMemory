6645fcfe42465f39203b87ae7dd6bfaf28cc377c
Tobias Rohner <trohner@daint104.login.cscs.ch>
2021-05-04 15:00:16 +0200

    MPI support for unit tests

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,7 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include "zisa/testing/testing_framework.hpp"
+#if ZISA_HAS_MPI
+#include <mpi.h>
+#endif
 
 int main( int argc, char* argv[] ) {
+#if ZISA_HAS_MPI
+  MPI_Init(&argc, &argv);
+#endif
   int result = Catch::Session().run( argc, argv );
+#if ZISA_HAS_MPI
+  MPI_Finalize();
+#endif
   return result;
 }
