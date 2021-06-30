fc876811e60620608e7597a577ecc49fb3ec6af8
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-06-30 18:07:32 +0200

    Remove test of implementation.

--- test/zisa/unit_test/memory/array_traits.cpp
+++ test/zisa/unit_test/memory/array_traits.cpp
@@ -9,11 +9,6 @@ namespace zisa {
 void test_array_traits() {
   int_t n_elements = 10;
 
-  using A = contiguous_memory<double>;
-  typename std::enable_if<std::is_same<decltype(std::declval<A>().device()), device_type>::value, device_type>::type foo = device_type::unknown;
-  REQUIRE(foo == device_type::unknown);
-
-
   // zisa::contiguous_memory
   auto alloc = allocator<double>(device_type::cpu);
   auto flat_memory = contiguous_memory<double>(n_elements, alloc);
