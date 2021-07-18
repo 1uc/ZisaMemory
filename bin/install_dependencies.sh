18bfeaaf23f2970348e713188cbe0874d0622ee6
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-07-18 10:02:49 +0200

    Use and rely on `find_dependency`.

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -91,7 +91,6 @@ do
 
     ${CMAKE} -DCMAKE_INSTALL_PREFIX="${install_dir}/zisa" \
              -DCMAKE_PREFIX_PATH="${install_dir}/zisa/lib/cmake/zisa" \
-             -DCMAKE_MODULE_PATH="${install_dir}/conan" \
              -DCMAKE_PROJECT_${dep}_INCLUDE="${install_dir}/conan/conan_paths.cmake" \
              -DCMAKE_C_COMPILER="${CC}" \
              -DCMAKE_CXX_COMPILER="${CXX}" \
@@ -108,12 +107,11 @@ echo "The dependencies were installed at"
 echo "    export DEP_DIR=${install_dir}"
 echo ""
 echo "Use"
-echo "    ${CMAKE} -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake " \
-     " -DCMAKE_MODULE_PATH=${install_dir}/conan " \
-     " -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa " \
-     " -DZISA_HAS_MPI=${ZISA_HAS_MPI} " \
-     " -DZISA_HAS_CUDA=${ZISA_HAS_CUDA} " \
-     " -DCMAKE_C_COMPILER=${CC} " \
-     " -DCMAKE_CXX_COMPILER=${CXX} " \
-     " REMAINING_ARGS "
+echo "    ${CMAKE} -DCMAKE_PROJECT_${component_name}_INCLUDE=${install_dir}/conan/conan_paths.cmake \ "
+echo "          -DCMAKE_PREFIX_PATH=${install_dir}/zisa/lib/cmake/zisa \ "
+echo "          -DZISA_HAS_CUDA=${ZISA_HAS_CUDA} \ "
+echo "          -DCMAKE_C_COMPILER=${CC} \ "
+echo "          -DCMAKE_CXX_COMPILER=${CXX} \ "
+echo "          -DCMAKE_BUILD_TYPE=FastDebug \ "
+echo "          -B build "
 
