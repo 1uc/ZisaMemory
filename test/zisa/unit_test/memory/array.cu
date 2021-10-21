2ee45c7adc7355052b8b489327edfc67bfc54a0a
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-10-21 07:22:18 +0200

    Add test to demonstrate `a.const_view()`.

--- test/zisa/unit_test/memory/array.cu
+++ test/zisa/unit_test/memory/array.cu
@@ -15,11 +15,19 @@ __global__ void call_kernel(array_view<double, 2> y, array<double, 2> x) {
   }
 }
 
+template <int NDIMS>
+static bool f(const array_const_view<double, NDIMS> &) {
+  return true;
+}
+
 TEST_CASE("array/array_view; API", "[cuda][array]") {
   auto a = array<double, 2>({3, 2}, device_type::cuda);
   auto b = array<double, 2>({3, 2}, device_type::cuda);
 
   auto b_view = array_view<double, 2>(b);
-
   call_kernel<<<1, 1>>>(b_view, a);
+  call_kernel<<<1, 1>>>(b.view(), a);
+
+  // f(b);  // template deduction fails.
+  f(b.const_view());
 }
