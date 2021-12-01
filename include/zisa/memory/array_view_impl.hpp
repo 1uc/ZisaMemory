e7c5929d6efc4c3e5a17a3e9497c10d9b9ae13ad
Luc Grosheintz <luc.grosheintz@gmail.com>
2021-12-01 17:23:43 +0100

    Fix clion 'refactoring'.

--- include/zisa/memory/array_view_impl.hpp
+++ include/zisa/memory/array_view_impl.hpp
@@ -4,7 +4,7 @@
 namespace zisa {
 
 template <class T>
-auto zisa::raw_ptr(T *a) -> decltype(a) {
+auto raw_ptr(T *a) -> decltype(a) {
   return a;
 }
 
@@ -33,17 +33,17 @@ array_view_base<T, Indexing>::shape(array_view_base::size_type i) const {
 }
 
 template <class T, class Indexing>
-device_type zisa::memory_location(const array_view_base<T, Indexing> &view) {
+device_type memory_location(const array_view_base<T, Indexing> &view) {
   return view.memory_location();
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 array_view<T, n_dims, Indexing>::array_view(const shape_t<n_dims> &shape,
                                             T *ptr,
                                             device_type mem_location)
     : super(shape, ptr, mem_location) {}
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 template <class Array, class Shape>
 array_view<T, n_dims, Indexing>::array_view(
     array_base<T, Indexing<n_dims>, Array, Shape> &other)
@@ -51,21 +51,21 @@ array_view<T, n_dims, Indexing>::array_view(
                  zisa::raw_ptr(other),
                  zisa::memory_location(other)) {}
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 array_view<T, n_dims, Indexing>::array_view(std::vector<T> &v)
     : array_view(shape_t<1>{v.size()}, v.data(), device_type::cpu) {}
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T *array_view<T, n_dims, Indexing>::raw() const {
   return this->_ptr;
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T &array_view<T, n_dims, Indexing>::operator[](array_view::size_type i) const {
   return this->_ptr[i];
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 template <class... Ints>
 T &array_view<T, n_dims, Indexing>::operator()(Ints... ints) const {
   auto l = Indexing<n_dims>::linear_index(this->shape(),
@@ -73,33 +73,33 @@ T &array_view<T, n_dims, Indexing>::operator()(Ints... ints) const {
   return (*this)[l];
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T *array_view<T, n_dims, Indexing>::begin() const {
   return this->_ptr;
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T *array_view<T, n_dims, Indexing>::end() const {
   return this->_ptr + this->size();
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T const *array_view<T, n_dims, Indexing>::cbegin() const {
   return this->_ptr;
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 T const *array_view<T, n_dims, Indexing>::cend() const {
   return this->_ptr + this->size();
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 void array_view<T, n_dims, Indexing>::copy_data(
     const array_view<T, n_dims, Indexing> &other) const {
   copy_data(array_const_view<T, n_dims, Indexing>(other));
 }
 
-template <class T, int n_dims, class Indexing>
+template <class T, int n_dims, template <int> class Indexing>
 void array_view<T, n_dims, Indexing>::copy_data(
     const array_const_view<T, n_dims, Indexing> &other) const {
   assert((*this).shape() == other.shape());
